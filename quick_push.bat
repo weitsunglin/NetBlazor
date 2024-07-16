55dfc86ec03a253ee89744d5ed5ee4b13705ef2f
weitsung <wilson.lwt.tw@gmail.com>
2024-07-16 23:08:11 +0800

    commit all files

--- quick_push.bat
+++ quick_push.bat
@@ -2,30 +2,30 @@
 
 REM Navigate to gameservice directory
 cd /d "%~dp0gameservice"
+rd /s /q bin
+rd /s /q obj
 
 REM Create .gitignore file and configure it to ignore bin and obj directories
 echo bin/ > .gitignore
 echo obj/ >> .gitignore
 
 REM Add .gitignore, commit changes, and push to the remote repository
-git add .gitignore
-git commit -m "Add .gitignore to ignore bin and obj directories"
 git add -A
-git commit -m "Add all files and initial commit"
+git commit -m "commit all files"
 git push -u origin main
 
 REM Navigate to services-gui directory
 cd /d "%~dp0services-gui"
+rd /s /q bin
+rd /s /q obj
 
 REM Create .gitignore file and configure it to ignore bin and obj directories
 echo bin/ > .gitignore
 echo obj/ >> .gitignore
 
 REM Add .gitignore, commit changes, and push to the remote repository
-git add .gitignore
-git commit -m "Add .gitignore to ignore bin and obj directories"
 git add -A
-git commit -m "Add all files and initial commit"
+git commit -m "commit all files"
 git push -u origin main
 
 echo Repositories configured and pushed successfully.
