b67b1c49b34e9ae8e60e16d2a76142d776d2f0b2
weitsung <wilson.lwt.tw@gmail.com>
2024-07-18 15:33:55 +0800

    commit log-service

--- quick_push.bat
+++ quick_push.bat
@@ -20,13 +20,13 @@ git commit -m "commit services-gui"
 git push origin main
 
 
-cd /d "%~dp0tcpClientExample"
+cd /d "%~dp0test-project\tcp-test"
 rd /s /q bin
 rd /s /q obj
 echo bin/ > .gitignore
 echo obj/ >> .gitignore
 git add -A
-git commit -m "commit tcpClientExample"
+git commit -m "commit test-project"
 git push origin main
 
 echo Repositories configured and pushed successfully.
