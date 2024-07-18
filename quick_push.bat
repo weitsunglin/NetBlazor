6c9545985c096917675f39ab127c4c5c25a15306
weitsung <wilson.lwt.tw@gmail.com>
2024-07-18 15:36:41 +0800

    commit a001

--- quick_push.bat
+++ quick_push.bat
@@ -1,33 +1,19 @@
 @echo off
 
-cd /d "%~dp0log-service"
-rd /s /q bin
-rd /s /q obj
-echo bin/ > .gitignore
-echo obj/ >> .gitignore
-git add -A
-git commit -m "commit log-service"
-git push origin main
+:: Define an array of project directories
+set "projects=a001 CommonLibrary services-gui test-project\tcp-test"
 
-
-cd /d "%~dp0services-gui"
-rd /s /q bin
-rd /s /q obj
-echo bin/ > .gitignore
-echo obj/ >> .gitignore
-git add -A
-git commit -m "commit services-gui"
-git push origin main
-
-
-cd /d "%~dp0test-project\tcp-test"
-rd /s /q bin
-rd /s /q obj
-echo bin/ > .gitignore
-echo obj/ >> .gitignore
-git add -A
-git commit -m "commit test-project"
-git push origin main
+:: Loop through each project directory
+for %%p in (%projects%) do (
+    cd /d "%~dp0%%p"
+    rd /s /q bin
+    rd /s /q obj
+    echo bin/ > .gitignore
+    echo obj/ >> .gitignore
+    git add -A
+    git commit -m "commit %%p"
+    git push origin main
+)
 
 echo Repositories configured and pushed successfully.
 pause
