55d6958ab1e8ec891f135faea830bacc4e0be77d
weitsung <wilson.lwt.tw@gmail.com>
2024-07-18 13:10:43 +0800

    commit log-service

--- quick_push.bat
+++ quick_push.bat
@@ -3,24 +3,31 @@
 cd /d "%~dp0log-service"
 rd /s /q bin
 rd /s /q obj
-
 echo bin/ > .gitignore
 echo obj/ >> .gitignore
-
 git add -A
-git commit -m "commit all files"
-git push -u origin main
+git commit -m "commit log-service"
+git push origin main
+
 
 cd /d "%~dp0services-gui"
 rd /s /q bin
 rd /s /q obj
-
 echo bin/ > .gitignore
 echo obj/ >> .gitignore
-
 git add -A
-git commit -m "commit all files"
-git push -u origin main
+git commit -m "commit services-gui"
+git push origin main
+
+
+cd /d "%~dp0tcpClientExample"
+rd /s /q bin
+rd /s /q obj
+echo bin/ > .gitignore
+echo obj/ >> .gitignore
+git add -A
+git commit -m "commit tcpClientExample"
+git push origin main
 
 echo Repositories configured and pushed successfully.
 pause
